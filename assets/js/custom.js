914f16f86180b6acae900eeeb7b7c880dd164298
Sovy0 <yanglx0530@gmail.com>
2023-10-30 14:16:12 +0800

    custom

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,6 @@
 var userAgent = navigator.userAgent;
-var API_BASE_URL = 'lecdapi.yaofan.site';
-var App_Key = '7iudhWquXYN0uToAuxseBFJe'
+var API_BASE_URL = 'donate.freelxy.site';
+var App_Key = 'reFNlSNCGmFLn0EDyN7pGFLq'
 
 // 判断是否在微信内
 if (userAgent.indexOf("MicroMessenger") !== -1) {
